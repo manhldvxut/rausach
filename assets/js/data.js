a4e34d3bbf9f5821e7f1b4d29d9b13cc9422e820
dh_lemanh <lemanh@demo.co.jp>
2021-10-19 19:00:03 +0900

    js news

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,21 @@
+var intViewportWidth = window.innerWidth;
+
+if(intViewportWidth < 769){
+  $(".news-row").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    prevArrow: '<div class="prev"><span>PREV</span></div>',
+    nextArrow: '<div class="next"><span>NEXT</span></div>'
+  });
+
+}
+
+
 $(".mvSlideArea").slick({
   autoplay: true,
   autoplaySpeed: 5000,
