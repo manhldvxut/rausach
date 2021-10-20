46aa7e6de59a2a730f539f79d255cc038fcb973b
dh_lemanh <lemanh@demo.co.jp>
2021-10-20 19:05:04 +0900

    form

--- contact/index.php
+++ contact/index.php
@@ -73,8 +73,88 @@ $page_shareurl = 'https://'.STORE_NAME.'.co.jp/';
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.5678081998126!2d105.68913851415418!3d18.683380487314366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cd721019f561%3A0xe3ef94596791189e!2zTFVEQUxBIC0gTcOzbiBuZ29uIMSQw6AgTOG6oXQ!5e0!3m2!1svi!2sjp!4v1634483546915!5m2!1svi!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 			</div>
 			<div class="tit-map"> <svg xmlns="http://www.w3.org/2000/svg" width="13.554" height="14" viewBox="0 0 13.554 14"><defs><style>.aff{fill:#362714;}</style>
-		</defs>
-		<path class="aff" d="M10.2,8.4,15.8,14H4.605Zm6.186-2.03v7.039l-5.6-5.6L11.91,6.7l-.064-.078c-.152-.185-.308-.375-.458-.576l-7.37,7.37V1.906a.277.277,0,0,1,.277-.277h6.738a3.553,3.553,0,0,1,6.539,1.926A4.333,4.333,0,0,1,16.389,6.372ZM9.474,4.463H7.418v.783H8.629A1.273,1.273,0,1,1,7.418,3.582a1.26,1.26,0,0,1,.775.263l.478-.621A2.037,2.037,0,0,0,7.418,2.8,2.056,2.056,0,1,0,9.474,4.854V4.463Zm7.265-.909a2.722,2.722,0,1,0-5.444,0c0,2.26,2.555,2.907,2.555,5.6h.333C14.184,6.462,16.739,5.814,16.739,3.554ZM14.017,2.309A1.061,1.061,0,1,0,15.078,3.37,1.061,1.061,0,0,0,14.017,2.309Z" transform="translate(-4.017)"></path></svg> <a href="https://g.page/ludala?share" target="_blank">Google Maps</a></div>
+				</defs>
+				<path class="aff" d="M10.2,8.4,15.8,14H4.605Zm6.186-2.03v7.039l-5.6-5.6L11.91,6.7l-.064-.078c-.152-.185-.308-.375-.458-.576l-7.37,7.37V1.906a.277.277,0,0,1,.277-.277h6.738a3.553,3.553,0,0,1,6.539,1.926A4.333,4.333,0,0,1,16.389,6.372ZM9.474,4.463H7.418v.783H8.629A1.273,1.273,0,1,1,7.418,3.582a1.26,1.26,0,0,1,.775.263l.478-.621A2.037,2.037,0,0,0,7.418,2.8,2.056,2.056,0,1,0,9.474,4.854V4.463Zm7.265-.909a2.722,2.722,0,1,0-5.444,0c0,2.26,2.555,2.907,2.555,5.6h.333C14.184,6.462,16.739,5.814,16.739,3.554ZM14.017,2.309A1.061,1.061,0,1,0,15.078,3.37,1.061,1.061,0,0,0,14.017,2.309Z" transform="translate(-4.017)"></path></svg> <a href="https://g.page/ludala?share" target="_blank">Google Maps</a></div>
+
+			<div class="contact--form">
+				<h2>Liên hệ - Hợp tác</h2>
+				<form class="contact__form">
+					<div class="form-items">
+						<div class="row contact__form-row">
+							<div class="col-lg-5 col-md-3">
+								<div class="contact__ttl">
+									<p>Họ và Tên</p>
+									<span class="requid">Required</span>
+								</div>
+							</div>
+							<div class="col-lg-7 col-md-9 contact__input">
+								<input class="input required__check" type="text" placeholder="Họ và Tên" required="">
+							</div>
+						</div>
+						<div class="row contact__form-row">
+							<div class="col-lg-5 col-md-3">
+								<div class="contact__ttl">
+									<p>Giới tính</p>
+									<span class="requid">Required</span>
+								</div>
+							</div>
+							<div class="col-lg-7 col-md-9 contact__input">
+								<div class="contact__ttl">
+									<div class="p-catalog--form-listChoice">
+										<div class="radio-item">
+											<input type="radio" id="radio01" name="radio-group">
+											<label for="radio01">Nam</label>
+										</div>
+										<div class="radio-item">
+											<input type="radio" id="radio02" name="radio-group">
+											<label for="radio02">Nữ</label>
+										</div>
+										<div class="radio-item">
+											<input type="radio" id="radio03" name="radio-group">
+											<label for="radio03">Khác</label>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row contact__form-row">
+							<div class="col-lg-5 col-md-3">
+								<div class="contact__ttl">
+									<p>Điện thoại</p>
+									<span class="requid">Required</span>
+								</div>
+							</div>
+							<div class="col-lg-7 col-md-9 contact__input">
+								<input class="input required__check" type="number" placeholder="Vui lòng Nhập số điện thoại" >
+							</div>
+						</div>
+						<div class="row contact__form-row">
+							<div class="col-lg-5 col-md-3">
+								<div class="contact__ttl">
+									<p>Email</p>
+								</div>
+							</div>
+							<div class="col-lg-7 col-md-9 contact__input">
+								<input class="input required__check" type="text" placeholder="Vui lòng Nhập Email" >
+							</div>
+						</div>
+						<div class="row contact__form-row">
+							<div class="col-lg-5 col-md-3 ">
+								<div class="contact__ttl mb-3">
+									<p>Nội dung trao đổi</p>
+									<span class="requid">Required</span>
+								</div>
+							</div>
+							<div class="col-lg-7 col-md-9 contact__input">
+								<textarea class="required__check" name="text" cols="80" rows="5" placeholder="Vui lòng nhập nội dung trao đổi" required=""></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="contact-btn text-center">
+						<a href="" class="btn-c ping">Gửi thông tin</a>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 </main>
